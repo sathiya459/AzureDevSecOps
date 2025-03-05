925f6f64a810950435c245b02b72a999ad417bf1
Sathya <sathiya0405@gmail.com>
2025-03-05 16:20:31 +0530

    storage account for tfstate added

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ terraform {
         version = "3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "terraform-rg"
-    storage_account_name = "terraformmagizhavan001"
-    container_name = "tfstatecontainer"
-    key = "dev.terraform.tfstate"
-  }
+#   backend "azurerm" {
+#     resource_group_name = "terraform-rg"
+#     storage_account_name = "terraformmagizhavan001"
+#     container_name = "tfstatecontainer"
+#     key = "dev.terraform.tfstate"
+#   }
 }
 provider "azurerm" {
   features {  }
