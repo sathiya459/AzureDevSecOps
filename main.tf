64027e3c8eca4122bf5db010b2bf2055c78a5e47
Sathya <sathiya0405@gmail.com>
2025-03-05 16:29:26 +0530

    Hardcoded keys

--- main.tf
+++ main.tf
@@ -5,14 +5,18 @@ terraform {
         version = "3.0.0"
     }
   }
-#   backend "azurerm" {
-#     resource_group_name = "terraform-rg"
-#     storage_account_name = "terraformmagizhavan001"
-#     container_name = "tfstatecontainer"
-#     key = "dev.terraform.tfstate"
-#   }
+  backend "azurerm" {
+    resource_group_name = "terraform-rg"
+    storage_account_name = "terraformmagizhavan001"
+    container_name = "tfstatecontainer"
+    key = "dev.terraform.tfstate"
+  }
 }
 provider "azurerm" {
   features {  }
+  client_id = "fc85d8f7-31cd-409b-bb2f-f97acbdfcb5d"
+  client_secret = "~ZQ8Q~MFmeCWjem203ac8hXgq.ne7W9YHyfUxdoz"
+  tenant_id = "63c1aede-f86c-422c-8898-842391a5917b"
+  subscription_id = "4316f0a8-5922-4e91-a324-89a55b9bc883"
 }
 
