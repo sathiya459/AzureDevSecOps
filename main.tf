68043ef571f8b318b28ffcdffe1fbf1e28382621
Sathya <sathiya0405@gmail.com>
2025-03-05 16:30:15 +0530

    Hardcoded keys

--- main.tf
+++ main.tf
@@ -14,8 +14,6 @@ terraform {
 }
 provider "azurerm" {
   features {  }
-  client_id = "fc85d8f7-31cd-409b-bb2f-f97acbdfcb5d"
-  client_secret = "~ZQ8Q~MFmeCWjem203ac8hXgq.ne7W9YHyfUxdoz"
   tenant_id = "63c1aede-f86c-422c-8898-842391a5917b"
   subscription_id = "4316f0a8-5922-4e91-a324-89a55b9bc883"
 }
